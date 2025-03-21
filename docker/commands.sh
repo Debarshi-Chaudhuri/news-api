6f74eab9d374fce6458b99ee0bc0bb40c6b75aea
Chaudhuri-Debarshi <debarshi@pice.one>
2025-03-21 17:10:04 +0530

    Compose command fix

--- docker/commands.sh
+++ docker/commands.sh
@@ -3,36 +3,36 @@ docker network create news-network
 
 # Run Elasticsearch Service
 cd docker
-docker-compose -f docker-compose.elasticsearch.yml up -d
+docker compose -f docker-compose.elasticsearch.yml up -d
 
 # Run News API Service
 cd docker
-docker-compose -f docker-compose.news-api.yml up -d
+docker compose -f docker-compose.news-api.yml up -d
 
 # Run Data Populator Service
 cd docker
-docker-compose -f docker-compose.data-populator.yml up -d
+docker compose -f docker-compose.data-populator.yml up -d
 
 # Run All Services Together
 cd docker
-docker-compose up -d
+docker compose up -d
 
 # Build and run specific services
 cd docker
-docker-compose -f docker-compose.elasticsearch.yml up -d --build  # Elasticsearch
-docker-compose -f docker-compose.news-api.yml up -d --build       # News API
-docker-compose -f docker-compose.data-populator.yml up -d --build # Data Populator
+docker compose -f docker-compose.elasticsearch.yml up -d --build  # Elasticsearch
+docker compose -f docker-compose.news-api.yml up -d --build       # News API
+docker compose -f docker-compose.data-populator.yml up -d --build # Data Populator
 
 # Stop services
 cd docker
-docker-compose down  # Stop all services
-docker-compose -f docker-compose.elasticsearch.yml down  # Stop Elasticsearch
-docker-compose -f docker-compose.news-api.yml down       # Stop News API
-docker-compose -f docker-compose.data-populator.yml down # Stop Data Populator
+docker compose down  # Stop all services
+docker compose -f docker-compose.elasticsearch.yml down  # Stop Elasticsearch
+docker compose -f docker-compose.news-api.yml down       # Stop News API
+docker compose -f docker-compose.data-populator.yml down # Stop Data Populator
 
 # View logs
 cd docker
-docker-compose logs -f  # All services
-docker-compose logs -f elasticsearch  # Elasticsearch only
-docker-compose logs -f news-api       # News API only
-docker-compose logs -f data-populator # Data Populator only
+docker compose logs -f  # All services
+docker compose logs -f elasticsearch  # Elasticsearch only
+docker compose logs -f news-api       # News API only
+docker compose logs -f data-populator # Data Populator only
